6e489a4f83a32df8f96c03205569f19add8e4d6a
ninjaboss69 <macabrepanpapakhin@gmail.com>
2025-08-16 21:02:24 +0630

    change unq

--- src/component/editor/index.js
+++ src/component/editor/index.js
@@ -14,7 +14,8 @@ function Editor({ setPostContent }) {
                     licenseKey: editorConfig.licenseKey,
                     plugins: editorConfig.plugins,
                     toolbar: editorConfig.toolbar,
-                    initialData: '<figure class="image image_resized" style="width:19.47%;"><img style="aspect-ratio:2520/1680;" src="http://localhost:5000/bucket/ninja-images/80da8667-1b77-4884-b699-0d07bea896e4-5a4030a0-cde2-45d0-be60-ac6d0f205acb-Photoon11-06-2025 at 11.24.jpg" width="2520" height="1680"></figure>',
+                    initialData: "",
+                    // initialData: '<figure class="image image_resized" style="width:19.47%;"><img style="aspect-ratio:2520/1680;" src="http://uniqenviorn.com:5000/bucket/ninja-images/80da8667-1b77-4884-b699-0d07bea896e4-5a4030a0-cde2-45d0-be60-ac6d0f205acb-Photoon11-06-2025 at 11.24.jpg" width="2520" height="1680"></figure>',
                     htmlSupport: {
                         allow: [
                             {
@@ -28,6 +29,7 @@ function Editor({ setPostContent }) {
                 }}
                 onChange={(event, editor) => {
                     const data = editor.getData();
+                    console.log("data ", data)
                     setPostContent("content", data);
                 }}
             />
